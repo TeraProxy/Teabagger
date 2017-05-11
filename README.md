95c225945abec4820d53cf0ad472b6752fe1c402
TeraProxy <siebenschwerter@aol.com>
2017-05-11 20:39:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# Teabagger
-A tera-proxy module that teabags people. Why bm?
-
-## Usage
-Type "!teabag" in an ingame whisper chat to start teabagging.
-Cancels after using another emote, skill or by simply moving.
+# Teabagger  
+A tera-proxy module that lets you teabag people. Why bm?  
+Activating the script changes your sit command (yes, the key works too) to a teabag.  
+You can adjust the speed by changing the TEABAGGING_DELAY value on the top of "index.js".  
+  
+## Usage  
+While in game, open a whisper chat session with "!Teabagger" by typing "/w !teabagger" in chat and hitting the space bar.
+This serves as the script's command interface. 
+The following commands are supported:  
+  
+* on - Enables the script  
+* off - Disables the script  
+  
+Any other input returns a summary of above commands in the game.
+  
+## Safety
+Whatever you send to "!Teabagger" in game is intercepted client-side. The chat is NOT sent to the server.
--- index.js
+++ index.js
@@ -1,29 +1,76 @@
+const TEABAGGING_DELAY = 100
 
 module.exports = function teabagger(dispatch) {
-	
-	let emoteNumber = 38 // sit
-	const RETRY_TIME = 800 // time to spam emote at in milliseconds
-	
-	let interval = null
+	let cid = null,
+		player = '',
+		interval = null,
+		sitting = false,
+		enabled = false
 
-	function doEmote() {
-		dispatch.toServer('C_SOCIAL', 1, { emote: emoteNumber, unk: 0 })
+	dispatch.hook('S_LOGIN', 1, event => {
+		({cid} = event)
+		player = event.name
+		enabled = false
+		sitting = false
+		interval = null
+	})
+
+	function teabag() {
+		if(sitting) {
+			dispatch.toServer('C_SOCIAL', 1, { emote: 39, unk: 0 })
+			sitting = false
+		}
+		else {
+			dispatch.toServer('C_SOCIAL', 1, { emote: 38, unk: 0 })
+			sitting = true
+		}
 	}
 	
+	dispatch.hook('C_SOCIAL', 1, event => { 
+		if(!enabled) return
+		if(event.emote == 38) {
+			clearInterval(interval)
+			interval = setInterval(teabag, TEABAGGING_DELAY)
+		}
+	})
+	
+	dispatch.hook('C_PLAYER_LOCATION', 1, () => { clearInterval(interval) })
+	dispatch.hook('C_PRESS_SKILL', 1, () => { clearInterval(interval) })
+	dispatch.hook('C_START_SKILL', 1, () => { clearInterval(interval) })
+	dispatch.hook('S_LOAD_TOPO', 1, () => { clearInterval(interval) })
+	dispatch.hook('S_RETURN_TO_LOBBY', 1, () => { clearInterval(interval) })
+	
+	// ################# //
+	// ### Chat Hook ### //
+	// ################# //
+	
 	dispatch.hook('C_WHISPER', 1, (event) => {
-		if (/^<FONT>!teabag?<\/FONT>$/i.test(event.message)) {
-			
-			emoteNumber = 38
-			interval = setInterval(doEmote, RETRY_TIME)
-
+		if(event.target.toUpperCase() === "!teabagger".toUpperCase()) {
+			if (/^<FONT>on?<\/FONT>$/i.test(event.message)) {
+				enabled = true
+				message('Teabagger <font color="#00EE00">enabled</font>.')
+			}
+			else if (/^<FONT>off?<\/FONT>$/i.test(event.message)) {
+				enabled = false
+				clearInterval(interval)
+				message('Teabagger <font color="#DC143C">disabled</font>.')
+			}
+			else message('Commands: "on" (changes normal sit to teabagging),'
+								+ ' "off" (change sit back to normal behavior)'
+						)
 			return false
 		}
 	})
 	
-	dispatch.hook('C_PLAYER_LOCATION', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote()  })
-	dispatch.hook('C_PRESS_SKILL', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote() })
-	dispatch.hook('C_SOCIAL', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote() })
-	dispatch.hook('C_START_SKILL', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote() })
-	dispatch.hook('S_LOAD_TOPO', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote() })
-	dispatch.hook('S_RETURN_TO_LOBBY', 1, () => { clearInterval(interval); emoteNumber = 0; doEmote() })
+	function message(msg) {
+		dispatch.toClient('S_WHISPER', 1, {
+			player: cid,
+			unk1: 0,
+			gm: 0,
+			unk2: 0,
+			author: '!Teabagger',
+			recipient: player,
+			message: msg
+		})
+	}
 }
