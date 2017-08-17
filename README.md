92c59d4ef558e17e4e1fe6af6909737d03240297
TeraProxy <siebenschwerter@aol.com>
2017-08-17 14:54:35 +0200

    Conversion to command, cleanup

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # Teabagger  
 A tera-proxy module that lets you teabag people. Why bm?  
 Activating the script changes your sit command (yes, the key works too) to a teabag.  
-You can adjust the speed by changing the TEABAGGING_DELAY value on the top of "index.js".  
+You can adjust the default speed by changing the TEABAGGING_DELAY value on the top of "index.js".  
   
 ## Usage  
-While in game, open a whisper chat session with "!Teabagger" by typing "/w !teabagger" in chat and hitting the space bar.
-This serves as the script's command interface. 
+While in game, open a proxy chat session by typing "/proxy" or "/8" in chat and hitting the space bar.  
+This serves as the script's command interface.  
 The following commands are supported:  
   
-* on - Enables the script  
-* off - Disables the script  
-* delay [x] - Changes delay between teabags to x in milliseconds, e.g. delay 100  
+* tbag - enable/disable Teabagger  
+* tbag [x] - change teabagging delay to x, e.g. "tbag 100"  
   
-Any other input returns a summary of above commands in the game.  
-  
-Alternative commands in all other chats:  
-* !tbag - Toggles between "on" and "off" state  
-* !tbagdelay [x] - Changes delay between teabags to x in milliseconds, e.g. !tbagdelay 100  
+Any other input, starting with "tbag", will return a summary of above commands in the chat.  
   
 ## Safety
-Whatever you send to "!Teabagger" in game is intercepted client-side. The chat is NOT sent to the server.
+Whatever you send to the proxy chat in game is intercepted client-side. The chat is NOT sent to the server.  
+  
+## Changelog
+### 1.2.0
+* [*] Some code cleanup
+* [*] Full conversion to Pinkie Pie's command module which is now a requirement
+### 1.1.0
+* [*] Improved teabagging algorithm
+* [+] Added !tbagdelay option
+### 1.0.0
+* [*] Initial Release
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+const Command = require('command')
+
 module.exports = function Teabagger(dispatch) {
-	let cid = null,
-		player = '',
-		interval = null,
+	let interval = null,
 		sitting = false,
 		enabled = false,
 		teabagging = false,
-		TEABAGGING_DELAY = 30	// Teabagging speed
+		TEABAGGING_DELAY = 80	// Teabagging delay in ms
 
-	dispatch.hook('S_LOGIN', 1, event => {
-		({cid} = event)
-		player = event.name
-		enabled = false
+	dispatch.hook('S_LOGIN', 2, event => {
 		sitting = false
 		teabagging = false
 		interval = null
 	})
-
+	
+	dispatch.hook('C_SOCIAL', 1, event => { 
+		clearInterval(interval)
+		if(enabled && event.emote == 38 && !teabagging) teabag()
+	})
+	
+	dispatch.hook('C_PLAYER_LOCATION', 1, () => { clearInterval(interval); teabagging = false })
+	dispatch.hook('C_PRESS_SKILL', 1, () => { clearInterval(interval); teabagging = false })
+	dispatch.hook('C_START_SKILL', 1, () => { clearInterval(interval); teabagging = false })
+	dispatch.hook('S_LOAD_TOPO', 1, () => { clearInterval(interval); teabagging = false })
+	dispatch.hook('S_RETURN_TO_LOBBY', 1, () => { clearInterval(interval); teabagging = false })
+	
 	function teabag() {
 		if(!enabled) return
 		teabagging = true
@@ -31,77 +39,24 @@ module.exports = function Teabagger(dispatch) {
 		}, TEABAGGING_DELAY)
 	}
 	
-	dispatch.hook('C_SOCIAL', 1, event => { 
-		clearInterval(interval)
-		if(enabled && event.emote == 38 && !teabagging) teabag()
-	})
-	
-	dispatch.hook('C_PLAYER_LOCATION', 1, () => { clearInterval(interval); teabagging = false })
-	dispatch.hook('C_PRESS_SKILL', 1, () => { clearInterval(interval); teabagging = false })
-	dispatch.hook('C_START_SKILL', 1, () => { clearInterval(interval); teabagging = false })
-	dispatch.hook('S_LOAD_TOPO', 1, () => { clearInterval(interval); teabagging = false })
-	dispatch.hook('S_RETURN_TO_LOBBY', 1, () => { clearInterval(interval); teabagging = false })
-	
 	// ################# //
 	// ### Chat Hook ### //
 	// ################# //
 	
-	dispatch.hook('C_WHISPER', 1, (event) => {
-		let cmd = null
-		if(event.target.toUpperCase() === "!teabagger".toUpperCase()) {
-			if (/^<FONT>on?<\/FONT>$/i.test(event.message)) {
-				enabled = true
-				message('Teabagger <font color="#56B4E9">enabled</font>.')
-			}
-			else if (/^<FONT>off?<\/FONT>$/i.test(event.message)) {
-				enabled = false
-				clearInterval(interval)
-				message('Teabagger <font color="#E69F00">disabled</font>.')
-			}
-			else if (cmd = /^<FONT>delay (.+?)<\/FONT>$/i.exec(event.message)) {
-				TEABAGGING_DELAY = Number(cmd[1])
-				message('Teabagger delay set to <font color="#F0E442">' + TEABAGGING_DELAY + '</font>.')
-			}
-			else message('Commands:<br>'
-								+ ' "on" (changes normal sit to teabagging),<br>'
-								+ ' "off" (change sit back to normal behavior),<br>'
-								+ ' "delay [x]" (change teabagging delay to x, e.g. delay 100)'
-						)
-			return false
+	const command = Command(dispatch)
+	command.add('tbag', (param) => {
+		if(param == null) {
+			enabled = !enabled
+			command.message('[Teabagger] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
+			console.log('[Teabagger] ' + (enabled ? 'enabled' : 'disabled'))
 		}
-	})
-	
-	function message(msg) {
-		dispatch.toClient('S_WHISPER', 1, {
-			player: cid,
-			unk1: 0,
-			gm: 0,
-			unk2: 0,
-			author: '!Teabagger',
-			recipient: player,
-			message: msg
-		})
-	}
-	
-	dispatch.hook('C_CHAT', 1, event => {
-		let cmd = null
-		if(/^<FONT>!tbag<\/FONT>$/i.test(event.message)) {
-			if(!enabled) {
-				enabled = true
-				message('Teabagger <font color="#56B4E9">enabled</font>.')
-				console.log('Teabagger enabled.')
-			}
-			else {
-				enabled = false
-				message('Teabagger <font color="#E69F00">disabled</font>.')
-				console.log('Teabagger disabled.')
-			}
-			return false
+		else if(param != null) {
+			TEABAGGING_DELAY = Number(param)
+			command.message('[Teabagger] delay set to <font color="#F0E442">' + TEABAGGING_DELAY + '</font>.')
 		}
-		else if (cmd = /^<FONT>!tbagdelay (.+?)<\/FONT>$/i.exec(event.message)) {
-			TEABAGGING_DELAY = Number(cmd[1])
-			message('Teabagger delay set to <font color="#F0E442">' + TEABAGGING_DELAY + '</font>.')
-			return false
-		}
+		else command.message('Commands:<br>'
+								+ ' "tbag" (enable/disable Teabagger),<br>'
+								+ ' "tbag [x]" (change teabagging delay to x, e.g. "tbag 100")'
+			)
 	})
 }
